42a55d5286f69c595f9a5f0cb771e846dc7bb429
Stephanie Moore <smoore_forestry@outlook.com>
2021-07-18 00:39:00 -0400

    amagram groups function complete

--- src/anagramGroups.js
+++ src/anagramGroups.js
@@ -2,6 +2,21 @@
  * Return a grouping of words by whether they are anagrams of each other or not
  * @param {array} words to be grouped
  */
-function anagramGroups(words) {}
+function anagramGroups(words) {
+  const wordsObj = {};
+
+  words.forEach((word) => {
+    let tempArray = word.toLowerCase().split("");
+    let sortedLetters = tempArray.sort().toString();
+
+    if (!wordsObj[sortedLetters]) {
+      wordsObj[sortedLetters] = [word];
+    } else {
+      wordsObj[sortedLetters] = [word, ...wordsObj[sortedLetters]];
+    }
+  })
+
+  return Object.values(wordsObj);
+}
 
 module.exports = anagramGroups;
