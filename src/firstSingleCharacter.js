0e8dbc002a46beb377b499303502dc9d078787cd
Stephanie Moore <smoore_forestry@outlook.com>
2021-07-18 13:00:01 -0400

    fist single character function, all tests NOT passing

--- src/firstSingleCharacter.js
+++ src/firstSingleCharacter.js
@@ -2,6 +2,24 @@
  * Return the first character in the string that occurs only once.
  * @param {string} word the string to be analysed
  */
-function firstSingleCharacter(word) {}
+function firstSingleCharacter(word) {
+  const wordArray = word.toLowerCase().split("");
+  const wordMap = new Map();
+
+  wordArray.forEach((letter) => {
+    if (wordMap.has(letter)) {
+      wordMap.set(letter, wordMap.get(letter) + 1)
+    } else {
+      wordMap.set(letter, 1)
+    };
+  })
+
+  wordMap.forEach((letter) => {
+    if (wordMap.get(letter) === 1) {
+      return letter;
+    }
+  })
+  return null;
+}
 
 module.exports = firstSingleCharacter;
--- src/permutationPalindrome.js
+++ src/permutationPalindrome.js
@@ -3,25 +3,12 @@
  * @param {string} word The word to check
  */
 function permutationPalindrome(word) {
-  word = word.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
-  let middle = Math.floor(word.length / 2);
-
-
-  const arr = [];
-
-  for (let index = 0; index < middle; index++) {
-    arr.push(word[index]);
+  let lowRegStr = word.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+  let reverseStr = lowRegStr.split('').reverse().join('');
+  if (reverseStr === lowRegStr) {
+    return true
   }
-
-  middle += word.length % 2 === 0 ? 0 : 1;
-
-  for (let index = middle, limit = word.length; index < limit; index++) {
-    if (word[index] !== arr.pop()) {
-      return false;
-    }
-  }
-
-  return true;
+  return false;
 
 }
 
